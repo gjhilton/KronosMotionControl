c9b348ec0e77f2ae6085b78b1d04d129c996aef0
gjhilton <gjh@robocoppelia.com>
2012-11-14 03:03:08 +0000

    driving and home tests

--- KronosMotionControlSimulator/KronosMotionControlSimulator/main.cpp
+++ KronosMotionControlSimulator/KronosMotionControlSimulator/main.cpp
@@ -24,15 +24,39 @@ void status(){
 	cout << "STATUS @" << motor.millis() << "ms -> " << home() << " Absolute: " << motor.getAbsoluteStep() << " Relative: " << motor.getRelativeStep() << "\n";
 }
 
+void testHome(){
+	cout << "\n\n***** TESTING GO HOME ****\n\n";
+	status();
+	motor.driveToAbsoluteTarget(20);
+	motor.driveToAbsoluteTarget(-100);
+	motor.setAtHome();
+	motor.driveByRelative(321);
+	motor.driveHome();
+	status();
+	if (motor.getRelativeStep() == 0 && motor.getAbsoluteStep() == -100 && motor.getHomePos() == -100){
+		cout << "\n\ntest passed.\n";
+	} else {
+		cout << "\n\n!!!! ERROR !!!!\n";
+	}
+}
+	
 void testDrive(){
+	cout << "\n\n**** TESTING DRIVE *****\n\n";
 	status();
-	// motor.setSpeed();
-	motor.drive(200);
-	motor.drive(-200);
+	motor.driveToAbsoluteTarget(200);
+	motor.driveToAbsoluteTarget(-200);
+	motor.driveByRelative(300);
+	motor.driveByRelative(-100);
 	status();
+	if (motor.getRelativeStep() == 0 && motor.getAbsoluteStep() == 0){
+		cout << "\n\ntest passed.\n";
+	} else {
+		cout << "\n\n!!!! ERROR !!!!\n";
+	}
 }
 
 int main(int argc, const char * argv[]) {
 	testDrive();
+	testHome();
     return 0;
 }
