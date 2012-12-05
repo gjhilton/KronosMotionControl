cb9da388fb1d208e6c88c261d536165f7b30da2c
gjhilton <gjh@robocoppelia.com>
2012-12-05 15:20:36 +0000

    begin new commands

--- KronosMotionControl/configOSC.h
+++ KronosMotionControl/configOSC.h
@@ -1,11 +1,23 @@
-#define OSC_ADDR_FORWARD 	"/ard/fwd"
-#define OSC_ADDR_REWIND 	"/ard/rwd"
-#define OSC_ADDR_SET_HOME 	"/ard/seth"
-#define OSC_ADDR_HOME 		"/ard/home"
+// MANUAL DRIVE
 
-#define OSC_ADDR_HARD_STOP	"/ard/fwd"
-#define OSC_ADDR_SET_01 	"/set1"
-#define OSC_ADDR_SET_12 	"/set2"
-#define OSC_ADDR_SET_23 	"/set3"
-#define OSC_ADDR_DEPLOY 	"/deploy"
-#define OSC_ADDR_UNDEPLOY 	"/undeploy"
+#define OSCADDR_FORWARD_STEP 		"/kronos/go/fwd"
+#define OSCADDR_REWIND_STEP 		"/kronos/go/back"
+#define OSCADDR_GO 					"/kronos/go/by"		// pass an integer value relative to current position
+#define OSCADDR_RESET				"/kronos/reset"		// rewinds until stopped (ie by home switch)
+
+// PERFORMANCE
+
+#define OSCADDR_DEPLOY 				"/kronos/perf/deploy"
+#define OSCADDR_UNDEPLOY 			"/kronos/perf/undeploy"
+#define OSCADDR_STUNT 				"/kronos/perf/stunt"
+
+// HOME
+
+#define OSCADDR_GO_HOME 			"/kronos/home/go"
+#define OSCADDR_SET_HERE_HOME 		"/kronos/home/here"
+#define OSCADDR_GO_HOME_PLUS		"/kronos/home/plus"		// pass an integer value relative to home
+
+// RELATIVE TO HOME
+
+#define OSCADDR_GO_KF 				"/kronos/kf/go"			// pass an integer keyframe index
+#define OSCADDR_SET_HERE_KF 		"/kronos/kf/here"
