301fd9d0fb7b38cb1e29dec831f226bf7b442cd2
gjhilton <gjh@robocoppelia.com>
2012-12-07 21:41:15 +0000

    backing off

--- KronosMotionControl/SerialController.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SerialController.cpp
-//  KronosMotionControlSimulator
-//
-//  Created by g j hilton on 07/12/2012.
-//  Copyright (c) 2012 hltn co. All rights reserved.
-//
-
-#include "SerialController.h"
